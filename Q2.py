6731fa90b0fac3f96455c485728888aa407a8247
hengvisoth <83302829+hengvisoth@users.noreply.github.com>
2021-10-18 15:38:54 +0700

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -10,46 +10,37 @@ if c < a and c < b:
     while True:
 
         n = n - ls[i]
-
+        if n < 0 :
+            break
         day += 1
         i += 1
         if i == 7:
             i = 0
-        if n < 0:
-            break
+       
 elif a < b and a < c:
     i = -2
     while True:
 
         n = n - ls[i]
+        if n < 0:
+            break
         day += 1
         i += 1
         if i == 7:
             i = 0
-        if n < 0:
-            break
+    
 elif b < a and b < c:
     i = 4
     while True:
 
         n = n - ls[i]
+        if n < 0:
+            break
         day += 1
         i += 1
         if i == 7:
             i = 0
-        if n < 0:
-            break
-print(day - 1)
 
-ls = [int(x) for x in input().split()]
-a = ls[0]
-b = ls[1]
-c = ls[2]
-mon = -c
-tue = -c
-wed = -a
-thu = -c
-fri = -b
-sat = - a
-sun = -c
-num_of_day = 0
+print(day )
+
+
